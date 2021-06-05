7a9ddcf2eafa1166d6e7f5b074ee8422f085e093
kabyabasu <kabyabasu@yahoo.co.uk>
2021-06-06 02:26:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+conda create -n wine python3.9 -y
+ctivate env
+conda activate wine
+reuirement.tx
+t
---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+dirs = [
+    os.path.join
+    ('data','raw'),
+    os.path.join('data','process'),
+    "data_given",
+
+    'notebooks',
+    'saved_models',
+    'src'
+
+]
+
+
+
+for dir_ in dirs:
+
+    os.makedirs(dir_,exist_ok=True)
+    with open(os.path.join(dir_,'.gitkeep'),"w") as f:
+        pass
+
+file_ = ['dvc.yml','params.ml','.gitignore',
+         os.path.join('src','__init__.py')]
+for i in file_:
+    with open(i,"w") as f:
+        pass
+
+
