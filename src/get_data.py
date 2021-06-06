b576752715b2b1aca57276dc825d52bf7bf10506
kabyabasu <kabyabasu@yahoo.co.uk>
2021-06-06 13:33:17 +0530

    updated reuirements.txt

---
+++ src/get_data.py
@@ -0,0 +1,18 @@
+#read params
+## process
+## return dataframe
+
+import os
+import yaml
+import pandas as pd
+import argparse
+
+
+def get_data():
+
+
+if __name__ = "__main__":
+    args = argparse.ArgumentParser()
+    args.add("--config",default="parms.yaml")
+    parsed_args = args.parse_args()
+
